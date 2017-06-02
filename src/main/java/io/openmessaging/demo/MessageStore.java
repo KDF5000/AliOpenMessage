02e8b7700f362413da59de02d2efe822c696f6f2
kdf5000 <kdf5000@163.com>
2017-06-03 01:05:49 +0800

    test no persistence

--- src/main/java/io/openmessaging/demo/MessageStore.java
+++ src/main/java/io/openmessaging/demo/MessageStore.java
@@ -42,26 +42,26 @@ class MessageFlush implements Runnable{
                 }
 //                System.out.println(Thread.currentThread().getName()+"take:"+queue.size());
 //                Message message = queue.take();
-                String queue = message.headers().getString(MessageHeader.QUEUE);
-                String topic = message.headers().getString(MessageHeader.TOPIC);
-                if ((topic == null && queue == null) || (topic != null && queue != null)) {
-                    throw new ClientOMSException(String.format("Queue:%s Topic:%s should put one and only one", true, queue));
-                }
-                String bucket = topic != null ? topic : queue;
-                int type = topic!=null ? Constant.TYPE_TOPIC : Constant.TYPE_QUEUE;
-
-                MappedFile mmapFile = null;
-                if(mmapFileMap.containsKey(type+bucket)){
-                    mmapFile = mmapFileMap.get(type+bucket);
-                }else{
-                    mmapFile = new MappedFile(this.storePath,bucket,type);
-                    mmapFileMap.put(type+bucket,mmapFile);
-                }
-                try{
-                    mmapFile.putMessage(message);
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
+//                String queue = message.headers().getString(MessageHeader.QUEUE);
+//                String topic = message.headers().getString(MessageHeader.TOPIC);
+//                if ((topic == null && queue == null) || (topic != null && queue != null)) {
+//                    throw new ClientOMSException(String.format("Queue:%s Topic:%s should put one and only one", true, queue));
+//                }
+//                String bucket = topic != null ? topic : queue;
+//                int type = topic!=null ? Constant.TYPE_TOPIC : Constant.TYPE_QUEUE;
+//
+//                MappedFile mmapFile = null;
+//                if(mmapFileMap.containsKey(type+bucket)){
+//                    mmapFile = mmapFileMap.get(type+bucket);
+//                }else{
+//                    mmapFile = new MappedFile(this.storePath,bucket,type);
+//                    mmapFileMap.put(type+bucket,mmapFile);
+//                }
+//                try{
+//                    mmapFile.putMessage(message);
+//                }catch (IOException e){
+//                    e.printStackTrace();
+//                }
                 count++;
                 if(count%1000000==0){
                     System.out.println("[KDF5000] count:"+count);
@@ -76,7 +76,7 @@ class MessageFlush implements Runnable{
 
 public class MessageStore {
     private static int MESSAGE_QUEUE_LEN = 200000;
-    private static int QUEUE_NUM = 40;
+    private static int QUEUE_NUM = 15; //2000w
     private static final MessageStore INSTANCE = new MessageStore();
     Thread[] ts = new Thread[QUEUE_NUM];
 
